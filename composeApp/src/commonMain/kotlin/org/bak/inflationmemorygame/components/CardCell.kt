78136a2733726e837eb7ae0e59e4badb9fba652d
Bakkuenkeeeen <255479769+Bakkuenkeeeen@users.noreply.github.com>
2026-01-20 09:37:25 +0900

    Improve card appearance

--- composeApp/src/commonMain/kotlin/org/bak/inflationmemorygame/components/CardCell.kt
+++ composeApp/src/commonMain/kotlin/org/bak/inflationmemorygame/components/CardCell.kt
@@ -2,12 +2,14 @@ package org.bak.inflationmemorygame.components
 
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -16,13 +18,18 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import inflationmemorygame.composeapp.generated.resources.Res
 import inflationmemorygame.composeapp.generated.resources.common_back
 import org.bak.inflationmemorygame.abilities.AbilityCard
 import org.jetbrains.compose.resources.painterResource
 
 private const val OFFSET_PX = -20f
+private val Shape = RoundedCornerShape(size = 8.dp)
 
 @Composable
 fun CardCell(modifier: Modifier = Modifier, card: AbilityCard?, onClick: () -> Unit) {
@@ -43,6 +50,7 @@ fun CardCell(modifier: Modifier = Modifier, card: AbilityCard?, onClick: () -> U
                 modifier = Modifier
                     .fillMaxHeight()
                     .aspectRatio(ratio = 0.5f, matchHeightConstraintsFirst = true)
+                    .clip(shape = Shape)
                     .align(Alignment.Center)
                     .clickable(
                         enabled = card.isInteractionEnabled && !card.isMatched,
