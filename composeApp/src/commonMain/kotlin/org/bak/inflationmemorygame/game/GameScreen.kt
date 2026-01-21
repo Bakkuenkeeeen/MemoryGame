e8c138562b778a545eacc1cc865d144d31a11a87
Bakkuenkeeeen <255479769+Bakkuenkeeeen@users.noreply.github.com>
2026-01-22 00:00:23 +0900

    Improve compact layout

--- composeApp/src/commonMain/kotlin/org/bak/inflationmemorygame/game/GameScreen.kt
+++ composeApp/src/commonMain/kotlin/org/bak/inflationmemorygame/game/GameScreen.kt
@@ -3,9 +3,14 @@ package org.bak.inflationmemorygame.game
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import org.bak.inflationmemorygame.components.BoardArea
 import org.bak.inflationmemorygame.components.LogArea
 import org.bak.inflationmemorygame.isWindowWidthCompact
@@ -22,13 +27,22 @@ fun GameScreen(state: GameStateViewModel = gameStateViewModel()) {
 
 @Composable
 fun CompactGameScreen(state: GameStateViewModel) {
-    BoardArea(
-        modifier = Modifier.fillMaxSize(),
-        stageState = state.currentStage,
-        cardsInEachRow = Params.CARDS_IN_EACH_ROW_COMPACT,
-        rowsInBoard = state.currentStage.cards.size / Params.CARDS_IN_EACH_ROW_COMPACT
-    ) {
-        state.onCardClick(card = it)
+    Column {
+        BoardArea(
+            modifier = Modifier.fillMaxWidth().weight(8f),
+            stageState = state.currentStage,
+            cardsInEachRow = Params.CARDS_IN_EACH_ROW_COMPACT,
+            rowsInBoard = state.currentStage.cards.size / Params.CARDS_IN_EACH_ROW_COMPACT
+        ) {
+            state.onCardClick(card = it)
+        }
+        Row(modifier = Modifier.weight(2f)) {
+            PlayerStatusArea(
+                modifier = Modifier.weight(1f).fillMaxSize(),
+                playerState = state.currentPlayer
+            )
+            LogArea(modifier = Modifier.weight(1f).fillMaxSize(), logs = state.messages)
+        }
     }
 }
 
@@ -52,7 +66,16 @@ fun WideGameScreen(state: GameStateViewModel) {
 
 @Composable
 fun PlayerStatusArea(modifier: Modifier = Modifier, playerState: PlayerState) {
-    Text("めくれる回数: ${playerState.flippedCount} / ${playerState.maxFlipCount}")
+    val fontColor by remember {
+        derivedStateOf {
+            if (playerState.isFlippable) Color.Unspecified else Color.Red
+        }
+    }
+    Text(
+        "めくれる回数: ${playerState.flippedCount} / ${playerState.maxFlipCount}",
+        modifier = modifier,
+        color = fontColor
+    )
 }
 
 @Composable
