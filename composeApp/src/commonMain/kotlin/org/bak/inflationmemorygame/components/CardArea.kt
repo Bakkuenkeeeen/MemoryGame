bc82be1029a3fdc7d11d27cdb5c296d92789be01
Bakkuenkeeeen <255479769+Bakkuenkeeeen@users.noreply.github.com>
2026-01-22 22:58:16 +0900

    Rotate card when clicked

--- composeApp/src/commonMain/kotlin/org/bak/inflationmemorygame/components/CardArea.kt
+++ composeApp/src/commonMain/kotlin/org/bak/inflationmemorygame/components/CardArea.kt
@@ -6,12 +6,12 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
@@ -25,12 +25,14 @@ import inflationmemorygame.composeapp.generated.resources.Res
 import inflationmemorygame.composeapp.generated.resources.common_back
 import org.bak.inflationmemorygame.abilities.AbilityCard
 import org.bak.inflationmemorygame.params.Params
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.painterResource
 
 private const val OFFSET_PX = -20f
 private val Shape = RoundedCornerShape(size = 8.dp)
-private val AnimationSpec: AnimationSpec<Float> =
-    tween(durationMillis = Params.CARD_APPEAR_ANIMATION_DURATION_MILLIS)
+
+private val RotationAnimationSpec: AnimationSpec<Float> =
+    tween(durationMillis = Params.CARD_FLIP_ANIMATION_DURATION_MILLIS)
 
 @Composable
 fun CardArea(
@@ -41,40 +43,68 @@ fun CardArea(
 ) {
     Box(modifier = modifier.fillMaxSize()) {
         if (animate) {
+            // 初期表示時に上から下へ、獲得時に下から上へカードを移動させるアニメーション
             var targetOffset by remember { mutableFloatStateOf(OFFSET_PX) }
+            val animationSpec: AnimationSpec<Float> = remember(card.isMatched) {
+                tween(
+                    durationMillis = Params.CARD_APPEAR_ANIMATION_DURATION_MILLIS,
+                    delayMillis = if (card.isMatched) Params.MATCHED_CARD_DISAPPEAR_DELAY_MILLIS else 0
+                )
+            }
             val offset by animateFloatAsState(
                 targetValue = targetOffset,
-                animationSpec = AnimationSpec
+                animationSpec = animationSpec
             )
             LaunchedEffect(card.isMatched) {
                 targetOffset = if (card.isMatched) OFFSET_PX else 0f
             }
-            Card(
-                modifier = Modifier.graphicsLayer {
+            // クリック時にカードを回転させるアニメーション
+            // 180度回転させた状態を表面にすると絵柄が反転してしまうので、180度→360度への回転が裏→表になるようにする
+            var targetRotation by remember { mutableFloatStateOf(180f) }
+            val rotation by animateFloatAsState(
+                targetValue = targetRotation,
+                animationSpec = RotationAnimationSpec
+            )
+            val isFaceUp by remember {
+                derivedStateOf { rotation > 270 }
+            }
+            LaunchedEffect(card.isFaceUp) {
+                targetRotation = if (card.isFaceUp) 360f else 180f
+            }
+            CardShapedImage(
+                modifier = modifier.align(Alignment.Center).graphicsLayer {
                     translationY = offset
                     alpha = 1f - (offset / OFFSET_PX).coerceIn(0f, 1f)
+                    rotationY = rotation
                 },
-                card = card,
+                illustration = if (isFaceUp) card.image else Res.drawable.common_back,
+                enabled = card.isInteractionEnabled && !card.isMatched,
                 onClick = onClick
             )
         } else {
-            Card(card = card, onClick = onClick)
+            CardShapedImage(
+                modifier = modifier.align(Alignment.Center),
+                illustration = if (card.isFaceUp) card.image else Res.drawable.common_back,
+                enabled = card.isInteractionEnabled && !card.isMatched,
+                onClick = onClick
+            )
         }
     }
 }
 
 @Composable
-private fun BoxScope.Card(modifier: Modifier = Modifier, card: AbilityCard, onClick: () -> Unit) {
+fun CardShapedImage(
+    modifier: Modifier = Modifier,
+    illustration: DrawableResource,
+    enabled: Boolean,
+    onClick: () -> Unit
+) {
     Image(
-        painter = painterResource(if (card.isFaceUp) card.image else Res.drawable.common_back),
+        painter = painterResource(illustration),
         contentDescription = null,
         modifier = modifier
             .aspectRatio(ratio = 0.5f)
             .clip(shape = Shape)
-            .align(Alignment.Center)
-            .clickable(
-                enabled = card.isInteractionEnabled && !card.isMatched,
-                onClick = onClick
-            )
+            .clickable(enabled = enabled, onClick = onClick)
     )
 }
