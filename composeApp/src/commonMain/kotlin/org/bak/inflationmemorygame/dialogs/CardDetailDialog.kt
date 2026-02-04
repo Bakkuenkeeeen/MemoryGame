f8a0f72c05c9951244156f955e3d86e399454fbd
Bakkuenkeeeen <255479769+Bakkuenkeeeen@users.noreply.github.com>
2026-02-04 12:40:54 +0900

    Improve detail dialog appearance

--- composeApp/src/commonMain/kotlin/org/bak/inflationmemorygame/dialogs/CardDetailDialog.kt
+++ composeApp/src/commonMain/kotlin/org/bak/inflationmemorygame/dialogs/CardDetailDialog.kt
@@ -2,10 +2,12 @@ package org.bak.inflationmemorygame.dialogs
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,18 +19,25 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.bak.inflationmemorygame.abilities.Ability
 import org.bak.inflationmemorygame.components.cardShape
+import org.bak.inflationmemorygame.isWindowWidthCompact
 import org.jetbrains.compose.resources.painterResource
 
 @Composable
 fun CardDetailDialog(modifier: Modifier, ability: Ability) {
     Row(modifier = modifier, verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painter = painterResource(ability.image),
-            contentDescription = null,
-            modifier = Modifier.aspectRatio(0.5f).cardShape()
-        )
+        if (!isWindowWidthCompact()) {
+            Spacer(modifier = Modifier.weight(0.5f))
+        }
+        Box(modifier = Modifier.weight(1f).fillMaxHeight()) {
+            Image(
+                painter = painterResource(ability.image),
+                contentDescription = null,
+                modifier = Modifier.aspectRatio(0.5f).cardShape().align(Alignment.Center)
+            )
+        }
         Spacer(modifier = Modifier.width(16.dp))
         Column(
+            modifier = Modifier.weight(1f),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
@@ -44,6 +53,9 @@ fun CardDetailDialog(modifier: Modifier, ability: Ability) {
                 color = Color.White,
                 fontWeight = FontWeight.Bold
             )
+        }
+        if (!isWindowWidthCompact()) {
+            Spacer(modifier = Modifier.weight(0.5f))
         }
     }
 }
